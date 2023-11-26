0e96c677e76917478b8f1daf8293fd1df8a44010
ktscates <cates2506@gmail.com>
2023-11-26 23:55:11 +0200

    sorting

--- app/components/Tasks.jsx
+++ app/components/Tasks.jsx
@@ -112,8 +112,8 @@ const Tasks = ({ setTasks, tasks }) => {
     if (
       a.status === "completed" &&
       b.status === "completed" &&
-      a.important &&
-      b.important
+      b.important &&
+      a.important
     ) {
       return b.id - a.id;
     }
